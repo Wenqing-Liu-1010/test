9ba3c0bfa66cb0a07913ba289ef6e83357ea1597
Wenqing Liu <wenqing.liu@usi.ch>
2024-09-22 14:55:08 +0200

    Update simcse_evaluation.py

--- simcse_evaluation.py
+++ simcse_evaluation.py
@@ -2,6 +2,7 @@ import torch
 from angle_emb import AnglE
 from angle_emb.utils import cosine_similarity
 import pandas as pd
+import numpy as np
 from transformers import AutoModel, AutoTokenizer
 class Metrics:
     def __init__(self, op_sd, op_sn, op_dn):
