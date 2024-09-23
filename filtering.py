802fd9d9bdebd254039ad4c2c95ae02d6b202d5a
Wenqing Liu <wenqing.liu@usi.ch>
2024-09-23 21:34:13 +0200

    Update filtering.py

--- filtering.py
+++ filtering.py
@@ -11,18 +11,18 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 # Loop through each dataset
 for split in splits:
     # Read the data
-    file_path = f'/mnt/lia/scratch/yifeng/dichotomous-score/data/delta_causal/{split}_processed_score_gt_07.jsonl'
+    file_path = f'/mnt/lia/scratch/yifeng/dichotomous-score/data/delta_causal/{split}_processed.jsonl'
     data = pd.read_json(file_path, lines=True)
 
     # Print the shape before filtering
     print(f"{split} dataset shape before filtering: {data.shape}")
 
     # Check if ". Hypothesis:" exists and add "." if needed
-    mask_hypothesis = data['context_text'].str.contains(r'\. Hypothesis:')
-    data.loc[~mask_hypothesis, 'context_text'] = data.loc[~mask_hypothesis, 'context_text'].str.replace('Hypothesis:', '. Hypothesis:')
+    mask_hypothesis = data['context_text'].str.contains(r'\. Effect:')
+    data.loc[~mask_hypothesis, 'context_text'] = data.loc[~mask_hypothesis, 'context_text'].str.replace('Effect:', '. Effect:')
 
     # Replace " . Hypothesis" with ". Hypothesis"
-    data['context_text'] = data['context_text'].str.replace(' . Hypothesis', '. Hypothesis')
+    data['context_text'] = data['context_text'].str.replace(' . Effect', '. Effect')
 
     # Extract neutral_id_prefix
     data['neutral_id_prefix'] = data['neutral_id'].str.extract(r'([^_]*_[^_]*)')[0]
@@ -37,8 +37,8 @@ for split in splits:
     # Print the shape after filtering
     print(f"{split} dataset shape after filtering: {filtered_data.shape}")
 
-    # Save the filtered data to a new file in a directory you have permission for
-    output_file_path = f'/mnt/lia/scratch/wenqliu/evaluation/delta_causal/{split}_processed_filtered_score_gt_07.jsonl'
-    filtered_data.to_json(output_file_path, orient='records', lines=True)
-    print(f"{split} dataset has been processed, results saved to: {output_file_path}")
+    # # Save the filtered data to a new file in a directory you have permission for
+    # output_file_path = f'/mnt/lia/scratch/wenqliu/evaluation/delta_causal/{split}_processed_filtered_score_gt_07.jsonl'
+    # filtered_data.to_json(output_file_path, orient='records', lines=True)
+    # print(f"{split} dataset has been processed, results saved to: {output_file_path}")
 
