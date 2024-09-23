4a7afc006a4d7881851fcf305e337786ad021a65
Wenqing Liu <wenqing.liu@usi.ch>
2024-09-23 21:27:41 +0200

    Update filtering.py

--- filtering.py
+++ filtering.py
@@ -3,15 +3,15 @@ import os
 import torch
 
 # Dataset names
-splits = ['train', 'dev', 'test']  # Modify based on actual dataset names
-
+#splits = ['train', 'dev', 'test']  # Modify based on actual dataset names
+splits = ['test']  # Modify based on actual dataset names
 # Ensure the device is set to CUDA if available
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # Loop through each dataset
 for split in splits:
     # Read the data
-    file_path = f'/mnt/lia/scratch/yifeng/dichotomous-score/data/defeasible_snli/{split}_processed.jsonl'
+    file_path = f'/mnt/lia/scratch/yifeng/dichotomous-score/data/delta_causal/{split}_processed_score_gt_07.jsonl'
     data = pd.read_json(file_path, lines=True)
 
     # Print the shape before filtering
@@ -38,7 +38,7 @@ for split in splits:
     print(f"{split} dataset shape after filtering: {filtered_data.shape}")
 
     # Save the filtered data to a new file in a directory you have permission for
-    output_file_path = f'/mnt/lia/scratch/wenqliu/evaluation/{split}_processed_filtered.jsonl'
+    output_file_path = f'/mnt/lia/scratch/wenqliu/evaluation/delta_causal/{split}_processed_filtered_score_gt_07.jsonl'
     filtered_data.to_json(output_file_path, orient='records', lines=True)
     print(f"{split} dataset has been processed, results saved to: {output_file_path}")
 
