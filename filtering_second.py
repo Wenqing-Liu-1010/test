75850c40e580102d9591e2505a382553431434b2
Wenqing Liu <wenqing.liu@usi.ch>
2024-10-06 15:57:24 +0200

    Update filtering_second.py

--- filtering_second.py
+++ filtering_second.py
@@ -7,13 +7,13 @@ def main():
     # 遍历每个数据集并打印数据条数
     for s in split:
         # 构建文件路径
-        file_path = f"/mnt/lia/scratch/yifeng/dichotomous-score/data_with_arrows_revised/delta_causal/{s}_filtered_with_arrows_pc.jsonl"
+        file_path = f"/mnt/lia/scratch/wenqliu/evaluation/delta_causal/regenerate/{split}.jsonl"
         
         # 读取 JSONL 文件
         df = pd.read_json(file_path, lines=True)
         
         # 打印数据条数
-        print(f"{s} 数据条数: {len(df)}")
+        print(f"{s} 数据数: {len(df)}")
 
 if __name__ == "__main__":
     main()
